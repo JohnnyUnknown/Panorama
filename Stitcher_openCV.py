89f8a50a1d255bec2f8be5ea881d9601edc0e146
Ivan <sloviane@mail.ru>
2025-02-27 17:35:04 +0400

    Незначительные правки

--- Stitcher_openCV.py
+++ Stitcher_openCV.py
@@ -1,6 +1,6 @@
 import cv2
 
-path_video = 'C:\\My\\Projects\\images\\Bol2.mp4'
+path_video = 'C:\\My\\Projects\\images\\move3.mp4'
 cap = cv2.VideoCapture(path_video)
 pano = []
 panorama = None
@@ -12,19 +12,20 @@ stitcher = cv2.Stitcher.create(cv2.Stitcher_SCANS)
 while cap.isOpened():
     frame_count += 1
     ret, frame = cap.read()
+    if len(pano_list) == 3:
+        break
 
     if not ret:
         print("Конец видеофайла.")
         break
-    if frame_count == 120:
+    if frame_count == 25:
         frame_count = 0
-        frame = cv2.resize(frame, (1024, 576))
+        frame = cv2.resize(frame, (1920, 1080))
 
-        # frame = frame[int(480/4):int(480-480/4), int(854/6):int(854-854/6)]
         im_list.append(frame)
 
         print(f"---"*10)
-        if len(im_list) == 5:
+        if len(im_list) == 3:
             status, pano = stitcher.stitch(im_list, cv2.Stitcher_SCANS)
             if status == cv2.Stitcher_OK:
                 print("OK")
@@ -37,13 +38,14 @@ while cap.isOpened():
                     print("Ошибка при оценке гомографии.")
                 else:
                     print("Ошибка во время сшивания:", status)
-                # break
+                break
 
 stitcher = cv2.Stitcher.create(cv2.Stitcher_SCANS)
-if len(im_list) > 0:
-    pano_list += im_list
+# if len(im_list) > 0:
+#     print("undo", len(pano_list))
+#     pano_list += im_list
+#     print("after", len(pano_list))
 status, panorama = stitcher.stitch(pano_list, cv2.Stitcher_SCANS)
-print(len(pano_list))
 if status == cv2.Stitcher_OK:
     print("OK Panorama")
     cv2.imwrite(f"pano_stitch.png", panorama)
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import cv2
 from PIL import Image
-import  math
-from decimal import *
 import numpy as np
 
 def rgba_to_grayscale_with_alpha(img):
@@ -16,34 +14,26 @@ def rgba_to_grayscale_with_alpha(img):
     gray_with_alpha = cv2.merge((gray_image, gray_image, gray_image, alpha_channel))
     return gray_with_alpha
 
-# def transparent_edges():
-#     image = Image.open(f"panorama.png").convert("RGBA")
-#     # Получаем данные пикселей
-#     data = image.getdata()
-#     new_data = []
-#     threshold = 1
-#     for item in data:
-#         # item - это кортеж (R, G, B, A)
-#         # Если пиксель черный (R, G, B все равны 0), делаем его прозрачным (A = 0)
-#         if item[0] < threshold and item[1] < threshold and item[2] < threshold:
-#             new_data.append((0, 0, 0, 0))  # Прозрачный пиксель
-#         else:
-#             new_data.append((item[2], item[1], item[0], 255))  # Оставляем пиксель без изменений
-#     # Обновляем данные изображения
-#     image.putdata(new_data)
-#     image = np.array(image)
-#     # image = rgba_to_grayscale_with_alpha(image)
-#     return image
+# Для устранения черных полос на панораме (черные пиксели делает прозрачными)
+def transparent_edges():
+    image = Image.open(f"panorama.png").convert("RGBA")
+    # Получаем данные пикселей
+    data = image.getdata()
+    new_data = []
+    threshold = 1
+    for item in data:
+        # item - это кортеж (R, G, B, A)
+        # Если пиксель черный (R, G, B все равны 0), делаем его прозрачным (A = 0)
+        if item[0] < threshold and item[1] < threshold and item[2] < threshold:
+            new_data.append((0, 0, 0, 0))  # Прозрачный пиксель
+        else:
+            new_data.append((item[2], item[1], item[0], 255))  # Оставляем пиксель без изменений
+    # Обновляем данные изображения
+    image.putdata(new_data)
+    image = np.array(image)
+    return image
 
 def detect_and_match_features(img1, img2):
-    # orb = cv2.ORB_create()
-    # keypoints1, descriptors1 = orb.detectAndCompute(img1, None)
-    # keypoints2, descriptors2 = orb.detectAndCompute(img2, None)
-    #
-    # bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-    # matches = bf.match(descriptors1, descriptors2)
-    # matches = sorted(matches, key=lambda x: x.distance)
-
     sift = cv2.SIFT_create()
     keypoints1, des1 = sift.detectAndCompute(img1, None)
     keypoints2, des2 = sift.detectAndCompute(img2, None)
@@ -53,7 +43,7 @@ def detect_and_match_features(img1, img2):
 
     good = []
     for m, n in matches:
-        if m.distance < 0.4 * n.distance:
+        if m.distance < 0.45 * n.distance:
             good.append(m)
 
     return keypoints1, keypoints2, good
@@ -66,16 +56,11 @@ def estimate_homography(keypoints1, keypoints2, matches, threshold=3):
     print(f"src:{len(src_points)}, dst:{len(dst_points)}")
 
     H, mask = cv2.findHomography(src_points, dst_points, cv2.RANSAC, threshold)
-
     return H, mask
 
 def warp_images(img1, img2, H):
     h1, w1 = img1.shape[:2]
     h2, w2 = img2.shape[:2]
-    # print(f"{h1=} {w1=}")
-    # cv2.imshow("img_new undo", img2)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
 
     corners1 = np.float32([[0, 0], [0, h1], [w1, h1], [w1, 0]]).reshape(-1, 1, 2)
     corners2 = np.float32([[0, 0], [0, h2], [w2, h2], [w2, 0]]).reshape(-1, 1, 2)
@@ -88,16 +73,8 @@ def warp_images(img1, img2, H):
     t = [-xmin, -ymin]
     Ht = np.array([[1, 0, t[0]], [0, 1, t[1]], [0, 0, 1]])
     # Ht_temp = np.array([[1, 0, t_all[0]], [0, 1, t_all[1]], [0, 0, 1]])
-    #
     # warped_img2_tmp = cv2.warpPerspective(img2, Ht_temp @ H, (xmax, ymax))
     warped_img2 = cv2.warpPerspective(img2, Ht @ H, (xmax - xmin, ymax - ymin))
-    # cv2.imshow("img_new after", warped_img2)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-
-    # cv2.imshow("img_temp", warped_img2_tmp)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
 
     return warped_img2, t
 
@@ -107,11 +84,7 @@ def image_overlay(image, warped_img, t):
     # Назначаем альфа-канал
     alpha_channel = image[:, :, 3] / 255.0  # Преобразуем 0-255 в 0-1
 
-    # image = transparent_edges()
-
-    # cv2.imshow("image", warped_img)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+    image = transparent_edges()
 
     # Наложение warped_img2 и image
     for c in range(4):  # Для каждого канала (R, G, B)
@@ -119,22 +92,16 @@ def image_overlay(image, warped_img, t):
                 alpha_channel * image[:, :, c] +
                 (1 - alpha_channel) * warped_img[t[1]:h1 + t[1], t[0]:w1 + t[0], c]
         )
-    # print(warped_img2.shape)
-    # cv2.imshow("Panorama", warped_img)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-
     return warped_img
 
 
 
-path_video = 'C:\\My\\Projects\\images\\Bol2.mp4'
+path_video = 'C:\\My\\Projects\\images\\move3.mp4'
 cap = cv2.VideoCapture(path_video)
 pano = []
 image_prev = []
 image_new = []
 frame_count = 0  # int(cap.get(cv.CAP_PROP_FPS))
-# t_all = [0, 0]
 stitcher = cv2.Stitcher.create(cv2.Stitcher_SCANS)
 
 while cap.isOpened():
@@ -144,13 +111,13 @@ while cap.isOpened():
     if not ret:
         print("Конец видеофайла.")
         break
-    if frame_count == 120:
+    if frame_count == 25:
         frame_count = 0
-        frame = cv2.resize(frame, (1024, 576))
-        # frame = frame[int(480/4):int(480-480/4), int(854/6):int(854-854/6)]
+        frame = cv2.resize(frame, (1920, 1080))
 
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
         # frame = rgba_to_grayscale_with_alpha(frame)
+
         if len(pano) == 0 and len(image_prev) == 0:
             print("new pano")
             pano = frame.copy()
@@ -159,11 +126,9 @@ while cap.isOpened():
             continue
 
         image_new = frame.copy()
-
         print(f"---"*10)
 
         keypoints1, keypoints2, matches = detect_and_match_features(image_prev, image_new)
-        print(f"kp1 {len(keypoints1)}, kp2 {len(keypoints2)}")
 
         try:
             H, mask = estimate_homography(keypoints1, keypoints2, matches)
@@ -171,7 +136,6 @@ while cap.isOpened():
             continue
 
         image_new, t_add = warp_images(image_prev, image_new, H)
-        # t_all = [t_all[i] + t_add[i] for i in range(2)]
 
         image_prev = image_new.copy()
 
